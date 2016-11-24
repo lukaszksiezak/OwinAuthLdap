c9e9969b7f88a2e3f8a65d84da8583fb8fdce598
Lukasz Ksiezak <lukasz.ksiezak@pl.abb.com>
2016-11-24 15:50:01 +0100

    initial commit for repo

---
+++ OwinAuth/ActiveDirectoryAuthMiddleware.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.Protocols;
+using System.Net;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace OwinAuth
+    {
+    using Microsoft.Owin;
+    using System.Security;
+    using AppFunc = Func<IDictionary<string, object>, Task>;
+
+    public class ActiveDirectoryAuthMiddleware
+        {
+        AppFunc _next;
+
+        public ActiveDirectoryAuthMiddleware(AppFunc next)
+            {
+            _next = next;
+            }
+
+        public async Task Invoke(IDictionary<string,Object> environment)
+            {
+            IOwinContext context = new OwinContext(environment);
+
+            var dumbUserName = "x";
+            var dumbUserPass = "xx";
+            if (ActiveDirectoryLogin(dumbUserName, dumbUserPass)) 
+                {
+                // _next is only invoked if authentication succeeds:
+                context.Response.StatusCode = 200;
+                context.Response.ReasonPhrase = "OK";
+                await _next.Invoke(environment);
+                }
+            else 
+                {
+                context.Response.StatusCode = 401;
+                context.Response.ReasonPhrase = "Not Authorized";
+
+                await context.Response.WriteAsync(string.Format("<h1>Error {0}-{1}",
+                    context.Response.StatusCode,
+                    context.Response.ReasonPhrase));
+                }
+            }
+
+        private bool ActiveDirectoryLogin(string userLogin, string userPassword)
+            {
+            try {
+                LdapConnection lcon = new LdapConnection(new LdapDirectoryIdentifier((string)null, false, false));
+                NetworkCredential nc = new NetworkCredential(userLogin, userPassword, Environment.UserDomainName);
+                lcon.Credential = nc;
+                lcon.AuthType = AuthType.Negotiate;
+                lcon.Bind(nc); // user has authenticated 
+
+                return true;
+                }
+            catch (LdapException) { //auth failed.
+                return false;
+                }
+            }
+        }
+    }
---
+++ OwinAuth/AppBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinAuth
+    {
+    public static class AppBuilderExtensions
+        {
+            public static void UseActiveDirectoryAuthMiddleware(this IAppBuilder app)
+            {
+            app.Use<ActiveDirectoryAuthMiddleware>();
+            }
+        }
+    }
